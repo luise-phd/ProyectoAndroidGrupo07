85f0077d75311319f35003a66b1cb29a8e8c865c
luise-phd <luis.ordonez.palacios@correounivalle.edu.co>
2021-10-27 19:53:02 -0500

    Se agrego la actividad 3

---
+++ app/src/main/java/com/ingluise/ProyectoAndroidGrupo07/ThirdActivity.java
@@ -0,0 +1,33 @@
+package com.ingluise.ProyectoAndroidGrupo07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+        //Activar boton volver en la ActionBar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //Finalizar la actividad
+    public void onBackPressed() {
+        finish();
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if(id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
+    }
+}
